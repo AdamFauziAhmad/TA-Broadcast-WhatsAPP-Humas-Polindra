9fa52c18689ed5d7776135b328f91a21f68cba3b
AdamFauziAhmad <adamfauziahmad1999@gmail.com>
2021-07-14 01:03:11 +0700

    fix bebrapa konfigurasi

--- application/config/config.php
+++ application/config/config.php
@@ -27,6 +27,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
 $newurl = str_replace("index.php", "", $_SERVER['SCRIPT_NAME']);
 $config['base_url']    = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
+// $config['base_url'] = 'https://generatebcwa.mymovement.online/';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,24 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'generatebcwa.mymovement.online',
+// 	'username' => 'mymove_generatewabc',
+// 	'password' => 'generatewabc',
+// 	'database' => 'mymove_generate_wabc',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
